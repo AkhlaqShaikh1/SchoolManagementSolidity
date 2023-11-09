6bcbf2619b75acaaed9f90e944c280ef1a77d4e5
AkhlaqShaikh1 <akhlaqshaikh03@gmail.com>
2023-11-09 18:56:03 +0500

    added fucntions to interact using js

--- contracts/school_management.sol
+++ contracts/school_management.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
+
 import "contracts/modifiers.sol";
+import "hardhat/console.sol";
 
 contract SchoolManagement is MyModifiers{
     struct StudentInformation{
@@ -12,21 +14,25 @@ contract SchoolManagement is MyModifiers{
     }
 
     address private admin;
-    mapping(uint => StudentInformation) students;
+    StudentInformation[] students;
 
     constructor(){
         admin = msg.sender;
     }
+
     event StudentAdded( uint timestamp);
     function addStudent(uint id, string memory name, string memory grade, uint batch) public {
     require(msg.sender == admin, "You are not admin!!");
     require(students[id].studentId != id, "Student Already Exists");
-
     // Create a new StudentInformation struct and add it to the mapping
-    students[id] = StudentInformation(name, id, grade, batch);
-
+    students.push( StudentInformation(name, id, grade, batch));
     emit StudentAdded(block.timestamp);
 }
+    function showAllStudents() external view returns(StudentInformation[] memory student){
+        require(students.length>0);
+        return student;
+    }
+
 
 
     
